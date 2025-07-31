61f0dcb97c98a74790e9556f8e065ed290388457
Carlos Bonilla <bonillagalindocarlos@gmail.com>
2025-07-30 21:41:30 -0600

    new base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/carlosBonillaGalindo/guitarla/' ,
+  base: 'https://carlosBonillaGalindo.github.io/guitarla/' ,
 })
